0d55665dde4f3d46ece0b3a9e1ee7ec28d5c9a50
“tiagorahal” <rahal.aires@gmail.com>
2022-01-03 08:59:31 -0300

    ESlint install

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
-let displayButton = document.getElementById('link-header-mobile-icon');//the button to change display to block
-let afterDisplayShowButton = document.getElementById('hidden-link-header');//the div that will trigger de display none
+const displayButton = document.getElementById('link-header-mobile-icon');// the button to change display to block
+const afterDisplayShowButton = document.getElementById('hidden-link-header');// the div that will trigger de display none
 
 function showHiddenDiv() {
-  if(document.getElementById('header-hidden-mobile').style.display = 'none') {
+  if (document.getElementById('header-hidden-mobile').style.display = 'none') {
     document.getElementById('header-hidden-mobile').style.display = 'block';
   }
 }
 
 function hideHiddenDiv() {
-  if(document.getElementById('header-hidden-mobile').style.display = 'block') {
+  if (document.getElementById('header-hidden-mobile').style.display = 'block') {
     document.getElementById('header-hidden-mobile').style.display = 'none';
   }
 }
