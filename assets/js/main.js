8513fa58763982ea5acc7d45a2954bcc1e31d1e9
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-10 12:11:40 -0300

    Update Stylelint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
 const displayButton = document.getElementById('link-header-mobile-icon');// the button to change display to block
 const afterDisplayShowButton = document.getElementById('hidden-link-header');// the div that will trigger de display none
 
-/* eslint-disable */
 function showHiddenDiv() {
-  if (document.getElementById('header-hidden-mobile').style.display = 'none') {
+  if (document.getElementById('header-hidden-mobile').style.display = 'none') { // eslint-disable-line
     document.getElementById('header-hidden-mobile').style.display = 'block';
   }
 }
 
 function hideHiddenDiv() {
-  if (document.getElementById('header-hidden-mobile').style.display = 'block') {
+  if (document.getElementById('header-hidden-mobile').style.display = 'block') { // eslint-disable-line
     document.getElementById('header-hidden-mobile').style.display = 'none';
   }
 }
-/* eslint-enable */
 
 displayButton.onclick = showHiddenDiv;
 afterDisplayShowButton.onclick = hideHiddenDiv;
