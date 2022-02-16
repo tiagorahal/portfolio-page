7b152a698af452e912752456aa8426c8e78f762b
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-16 01:25:16 -0300

    Linters issues fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable */ 
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,5 @@
 // A mock function to mimic making an async request for data
+/* eslint-disable */ 
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
+  return new Promise((resolve) => setTimeout(() => resolve({ data: amount }), 500));
 }
