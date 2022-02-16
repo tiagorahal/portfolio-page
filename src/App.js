bb4ab20e7804f554a22bc37f31af6809408489bb
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-16 02:02:39 -0300

    Add queries.css file for screen sizes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/header/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/queries.css';
 import './App.css';
+import './styles/header.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>THIS IS A TEST </h1>
+    <div className="App container-fluid">
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="Header row">
+      <div className="header-left col-2">
+        <h1>Tiago Rahal</h1>
+      </div>
+      <div className="header-right col">
+        <a href="/">testing link</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
