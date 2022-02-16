29507f005184e7884030694370b8779b92b6963f
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-16 03:07:54 -0300

    Align header title on mobile

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { MdOutlineEmail } from 'react-icons/md';
+import { AiOutlineBars } from 'react-icons/ai';
 
 function Header() {
   return (
     <div className="Header nav row align-items-center">
-      <div className="header-left nav col-lg-4 col-md-6 col-sm-8">
+      <div className="header-left nav col-lg-4 col-md-6 col-sm">
         <h1>Tiago Rahal</h1>
       </div>
-      <div className="header-right nav col-lg-7 col-md-6 justify-content-end">
+      <div className="header-right nav col-lg-7 col-md-6 col-sm justify-content-end">
         <div className="links-mobile">
           <a href="/">Portfolio</a>
           <a href="/">About</a>
@@ -17,9 +18,6 @@ function Header() {
           </a>
         </div>
       </div>
-      <a className="header-right-mobile" href="/">
-        <MdOutlineEmail />
-      </a>
     </div>
   );
 }
