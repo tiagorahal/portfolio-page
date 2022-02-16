3a4db9b636a1b4544a99b0fa65a5a0a290884638
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-16 02:35:43 -0300

    Add React icons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import MailHeader from '../../assets/icons/header-mail.svg';
 
 function Header() {
   return (
-    <div className="Header row">
-      <div className="header-left col-2">
+    <div className="Header nav row align-items-center">
+      <div className="header-left nav col-lg-4 justify-content-center">
         <h1>Tiago Rahal</h1>
       </div>
-      <div className="header-right col">
-        <a href="/">testing link</a>
+      <div className="header-right nav col justify-content-end">
+        <a href="/">Portfolio</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">{MailHeader}</a>
       </div>
     </div>
   );
