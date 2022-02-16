fbd42433f9c4eeeb265ceb316dd85eaed516eeb8
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-16 02:58:39 -0300

    Finish header for tablet or bigger sizes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,17 +4,22 @@ import { MdOutlineEmail } from 'react-icons/md';
 function Header() {
   return (
     <div className="Header nav row align-items-center">
-      <div className="header-left nav col-lg-4 justify-content-center">
+      <div className="header-left nav col-lg-4 col-md-6 col-sm-8">
         <h1>Tiago Rahal</h1>
       </div>
-      <div className="header-right nav col-lg-7 justify-content-end">
-        <a href="/">Portfolio</a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
-        <a href="/">
-          <MdOutlineEmail />
-        </a>
+      <div className="header-right nav col-lg-7 col-md-6 justify-content-end">
+        <div className="links-mobile">
+          <a href="/">Portfolio</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">
+            <MdOutlineEmail />
+          </a>
+        </div>
       </div>
+      <a className="header-right-mobile" href="/">
+        <MdOutlineEmail />
+      </a>
     </div>
   );
 }
