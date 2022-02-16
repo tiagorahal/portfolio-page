40d830d96314e22b481c24278ec305030c2fafa1
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-16 03:19:51 -0300

    Finish title and desktop links hidden on mobile

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,8 +5,11 @@ import { AiOutlineBars } from 'react-icons/ai';
 function Header() {
   return (
     <div className="Header nav row align-items-center">
-      <div className="header-left nav col-lg-4 col-md-6 col-sm">
-        <h1>Tiago Rahal</h1>
+      <div className="header-left nav col-lg-4 col-md-6 col-sm-6 align-items-center">
+        <h1 className="col">Tiago Rahal</h1>
+        <div className="header-right-mobile col-sm-4">
+          <AiOutlineBars />
+        </div>
       </div>
       <div className="header-right nav col-lg-7 col-md-6 col-sm justify-content-end">
         <div className="links-mobile">
