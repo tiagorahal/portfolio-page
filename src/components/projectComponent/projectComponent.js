e92d4e925ecb63305ef6c3cbbb6c61f6a07cc6be
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-17 12:06:38 -0300

    Finish all elements inside project component, fix positioning and sizes, set background mock image

--- src/components/projectComponent/projectComponent.js
+++ src/components/projectComponent/projectComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-template */
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
@@ -5,39 +6,50 @@ import React from 'react';
 
 const mockData = [
   {
-    title: 'I AM TESTING THIS 01',
+    projectTitle: 'I AM TESTING THIS 01',
+    projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
   },
   {
-    title: 'I AM TESTING THIS 02',
+    projectTitle: 'I AM TESTING THIS 02',
+    projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
   },
   {
-    title: 'I AM TESTING THIS 03',
+    projectTitle: 'I AM TESTING THIS 03',
+    projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
   },
   {
-    title: 'I AM TESTING THIS 04',
+    projectTitle: 'I AM TESTING THIS 04',
+    projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
   },
   {
-    title: 'I AM TESTING THIS 05',
-  },
-  {
-    title: 'I AM TESTING THIS 06',
+    projectTitle: 'I AM TESTING THIS 05',
+    projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
   },
 ];
 
 function ReturnProjects(props) {
   return (
-    <div className="inner-content nav col-sm-12 col-md-6 col-lg-4 justify-content-center">
-      <h2>{props.title}</h2>
+    <div className="inner-content justify-content-start">
+      <p className="project-title">{props.projectTitle}</p>
+      <p className="project-text">{props.projectText}</p>
+      <div className="buttons-div-component">
+        <button className="toolButton-component" type="button">Bootstrap</button>
+        <button className="toolButton-component" type="button">Javascript</button>
+        <button className="toolButton-component" type="button">React</button>
+        <button className="toolButton-component" type="button">Redux</button>
+      </div>
+      <a className="project-button-component" href="/">See Project</a>
     </div>
   );
 }
 
 function ProjectComponent() {
   return (
-    <div className="ProjectComponent row nav d-flex">
+    <div className="ProjectComponent row nav d-flex align-items-end justify-content-end">
       {mockData.map((returnprojects) => (
         <ReturnProjects
-          title={returnprojects.title}
+          projectTitle={returnprojects.projectTitle}
+          projectText={returnprojects.projectText}
         />
       ))}
     </div>
