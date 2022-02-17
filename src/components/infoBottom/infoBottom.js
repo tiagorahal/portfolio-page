5bc3b959184539e981c0edb9d9deaac4d187a2c5
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-17 18:39:44 -0300

    Add final elements on info container, adjust sizes and positioning

--- src/components/infoBottom/infoBottom.js
+++ src/components/infoBottom/infoBottom.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { ImTerminal } from 'react-icons/im';
 
 function InfoBottom() {
   return (
     <div className="InfoBottom">
-
+      <div className="info-bottom-column">
+        <h3 className="info-bottom-title d-flex flex-row">
+          <ImTerminal />
+          &#160;
+          Languages
+        </h3>
+        <p className="info-bottom-text">
+          Javascript
+          <br />
+          Ruby
+        </p>
+      </div>
+      <div className="info-bottom-column">
+        <h3 className="info-bottom-title d-flex flex-row">
+          <ImTerminal />
+          &#160;
+          Frameworks
+        </h3>
+        <p className="info-bottom-text">
+          React
+          <br />
+          Redux
+          <br />
+          Bootstrap
+          <br />
+          Ruby on Rails
+        </p>
+      </div>
+      <div className="info-bottom-column">
+        <h3 className="info-bottom-title d-flex flex-row">
+          <ImTerminal />
+          &#160;
+          Skills
+        </h3>
+        <p className="info-bottom-text">
+          Github
+          <br />
+          PostgreSQL
+          <br />
+          API
+          <br />
+          Terminal
+        </p>
+      </div>
     </div>
   );
 }
+
+export default InfoBottom;
--- src/containers/infoContainer/infoContainer.js
+++ src/containers/infoContainer/infoContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import InfoBottom from '../../components/infoBottom/infoBottom';
 
 const infoText = 'Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.';
 
@@ -17,6 +18,7 @@ function InfoContainer() {
         </button>
       </div>
       <hr />
+      <InfoBottom />
     </div>
   );
 }
