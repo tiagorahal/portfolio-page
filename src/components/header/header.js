a380f3b9fdc52757553abba12e8557054e9dcd87
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-17 19:14:59 -0300

    Add path to project link on header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,7 +13,7 @@ function Header() {
       </div>
       <div className="header-right nav col-lg-7 col-md-6 col-sm justify-content-end">
         <div className="links-desktop">
-          <a href="/">Portfolio</a>
+          <a href="#project-container">Portfolio</a>
           <a href="/">About</a>
           <a href="/">Contact</a>
           <a className="email-icon" href="/">
