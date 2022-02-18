2cf60c69051b71a7926ac833bae7a2dfe437088a
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-18 10:20:59 -0300

    Adjust positioning of project elements

--- src/components/projectComponent/projectComponent.js
+++ src/components/projectComponent/projectComponent.js
@@ -6,22 +6,24 @@ import React from 'react';
 
 const mockDataOne = [
   {
-    projectTitle: 'Covid News App',
-    projectText: 'This is a mobile web app the provides daily updates on the global spread of covid-19. The data is sourced from the Narrativa Data API. The project is build with React a JavaScript library, and Redux for state management.',
-    projectClass: 'projectOne inner-content justify-content-start',
-    firstButton: 'CSS',
-    secondButton: 'JavaScript',
-    thirdButton: 'React',
-    fourthButton: 'Redux',
+    projectTitle: 'Awesome Restaurants',
+    projectText: 'Check your region top restaurants and make reservations to eat in them! Awesome Restaurants uses a Rails-powered API to send and receive information in the React front-end',
+    projectClass: 'projectOne inner-content d-flex flex-column justify-content-end p-1',
+    firstButton: 'JavaScript',
+    secondButton: 'Ruby',
+    thirdButton: 'React & Redux',
+    fourthButton: 'Ruby on Rails',
+    projectLink: 'https://github.com/tiagorahal/awesome-resturants',
   },
   {
     projectTitle: 'I AM TESTING THIS 02',
     projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
-    projectClass: 'projectTwo inner-content justify-content-start',
+    projectClass: 'projectTwo inner-content d-flex flex-column justify-content-end p-1',
     firstButton: 'CSS',
     secondButton: 'JavaScript',
     thirdButton: 'React',
     fourthButton: 'Redux',
+    projectLink: 'https://github.com/tiagorahal/awesome-resturants',
   },
 ];
 
@@ -29,20 +31,22 @@ const mockDataTwo = [
   {
     projectTitle: 'I AM TESTING THIS 03',
     projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
-    projectClass: 'projectThree inner-content justify-content-start',
+    projectClass: 'projectThree inner-content d-flex flex-column justify-content-end p-1',
     firstButton: 'CSS',
     secondButton: 'JavaScript',
     thirdButton: 'React',
     fourthButton: 'Redux',
+    projectLink: 'https://github.com/tiagorahal/awesome-resturants',
   },
   {
     projectTitle: 'I AM TESTING THIS 04',
     projectText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ligula vitae dui molestie tristique blandit a nibh. Fusce id lorem tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. ',
-    projectClass: 'projectFour inner-content justify-content-start',
+    projectClass: 'projectFour inner-content d-flex flex-column justify-content-end p-1',
     firstButton: 'CSS',
     secondButton: 'JavaScript',
     thirdButton: 'React',
     fourthButton: 'Redux',
+    projectLink: 'https://github.com/tiagorahal/awesome-resturants',
   },
 
 ];
@@ -58,7 +62,7 @@ function ReturnProjects(props) {
         <button className="toolButton-component" type="button">{props.thirdButton}</button>
         <button className="toolButton-component" type="button">{props.fourthButton}</button>
       </div>
-      <a className="project-button-component" href="/">See Project</a>
+      <a className="project-button-component text-center" href={props.projectLink} target="_blank" rel="noreferrer">See Project</a>
     </div>
   );
 }
@@ -75,6 +79,7 @@ function ProjectComponentOne() {
           secondButton={returnprojects.secondButton}
           thirdButton={returnprojects.thirdButton}
           fourthButton={returnprojects.fourthButton}
+          projectLink={returnprojects.projectLink}
         />
       ))}
     </div>
@@ -93,6 +98,7 @@ function ProjectComponentTwo() {
           secondButton={returnprojects.secondButton}
           thirdButton={returnprojects.thirdButton}
           fourthButton={returnprojects.fourthButton}
+          projectLink={returnprojects.projectLink}
         />
       ))}
     </div>
