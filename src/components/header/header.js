3ee5e9e58d71e923d72d081c7a81a0e243caa6ca
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-22 11:25:42 -0300

    Add hidden div

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,7 +8,9 @@ function Header() {
       <div className="header-left nav col-lg-4 col-md-6 col-sm-6 align-items-center">
         <h1 className="col">Tiago Rahal</h1>
         <div className="header-right-mobile col-sm-4">
-          <AiOutlineBars />
+          <button className="header-right-mobile-button" type="button">
+            <AiOutlineBars />
+          </button>
         </div>
       </div>
       <div className="header-right nav col-lg-7 col-md-6 col-sm justify-content-end">
@@ -20,6 +22,9 @@ function Header() {
             <MdOutlineEmail />
           </a>
         </div>
+      </div>
+      <div className="hidden-mobile-div">
+        <h1>TESTING THIS THING HERE, BE PLEASE WAIT</h1>
       </div>
     </div>
   );
