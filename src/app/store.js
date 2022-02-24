9ef3f5f420fc76c57a7797b233221b79493a8c62
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-24 11:30:25 -0300

    Remove errors fron console on browser

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */ 
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
